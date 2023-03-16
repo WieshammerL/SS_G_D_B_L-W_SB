c7701f3dae51b4caa59f930a95a04fd0553ecf81
WieshammerL <106739638+WieshammerL@users.noreply.github.com>
2023-03-16 10:39:44 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,19 +4,54 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Test</h1>`;
+/*
+//Aufgabe 1
 let user = {};
 user.name = "John";
 user.surname = "Smith";
 user.name = "Pete";
 delete user.name;
+*/
 
+/*
+//Aufgabe 2
 function isEmpty(object){
   for(let temp in object){
-    return true;
+    return false;
   }
-  return false;
+  return true;
 }
 let a = {};
 //a.b = "hallo";
 console.log(isEmpty(a));
+*/
 
+/*
+//Aufgabe 3
+const x = {
+name: "John"
+};
+x.name = "Pete";
+console.log(x.name);
+*/
+
+/*
+//Aufgabe 4
+let salaries = {
+  John: 100,
+  Pete: 160,
+  Ann: 130,
+  Sepp: 80,
+  Ralph: -10
+};
+
+function getSum(object){
+  let sum = 0;
+  for(let key in object){
+      sum = sum + object[key];
+  }
+  return sum;
+}
+
+console.log(getSum(salaries));
+*/
